42869ea2477bd8242ae0576550450696b8dc93d6
dwehringer <36534515+dwehringer@users.noreply.github.com>
2019-03-02 20:33:42 +0100

    init

---
+++ precache-manifest.62c84a5a98cbefa6b1a06e1f939c4c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d140b9964e4a1eb975d",
+    "url": "/static/js/main.3d140b99.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "3d140b9964e4a1eb975d",
+    "url": "/static/css/main.979cc0ec.chunk.css"
+  },
+  {
+    "revision": "64d105e8efe2a64312ee9a1b3ca39221",
+    "url": "/index.html"
+  }
+];
