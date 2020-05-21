1a416d3c37f74f705042bfaa0db085aad4565011
dwehringer <36534515+dwehringer@users.noreply.github.com>
2020-05-21 20:54:35 +0200

    3

--- precache-manifest.cdd1bed7a3be6ba6ac68fbe63dd664cd.js
+++ precache-manifest.cdd1bed7a3be6ba6ac68fbe63dd664cd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "ce2639dc6a21aceeadf1",
-    "url": "/static/js/main.ce2639dc.chunk.js"
+    "revision": "79d68c6f8a338cedac4b",
+    "url": "/static/js/main.79d68c6f.chunk.js"
   },
   {
     "revision": "f03e8ae863144a61c620",
     "url": "/static/js/1.f03e8ae8.chunk.js"
   },
   {
-    "revision": "ce2639dc6a21aceeadf1",
+    "revision": "79d68c6f8a338cedac4b",
     "url": "/static/css/main.45bb0ef1.chunk.css"
   },
   {
-    "revision": "42ff763092f91b0f5dcd088b3405b6a4",
+    "revision": "825e1d6cf43a865233c9cdd8a4b88fa2",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86475074c2e8139d3e9aaff83d170a3b.js"
+  "/precache-manifest.cdd1bed7a3be6ba6ac68fbe63dd664cd.js"
 );
 
 workbox.clientsClaim();
