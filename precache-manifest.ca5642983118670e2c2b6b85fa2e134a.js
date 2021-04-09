1ceda320d4798cbe87d09af8bcc75e1e9d6369ce
dwehringer <36534515+dwehringer@users.noreply.github.com>
2021-04-09 21:32:11 +0200

    neu

---
+++ precache-manifest.ca5642983118670e2c2b6b85fa2e134a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a165f13a00dce18850c",
+    "url": "/static/js/main.2a165f13.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "2a165f13a00dce18850c",
+    "url": "/static/css/main.45bb0ef1.chunk.css"
+  },
+  {
+    "revision": "add10ceac7579fc1782587ba89e10fc3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd7aa7cb1ed38c6a58cf2e28fae3a861.js"
+  "/precache-manifest.ca5642983118670e2c2b6b85fa2e134a.js"
 );
 
 workbox.clientsClaim();
