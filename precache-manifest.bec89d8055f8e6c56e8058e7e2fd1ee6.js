561cf8638f73110e3d4209fe5d2cc3f2bf778e3c
dwehringer <36534515+dwehringer@users.noreply.github.com>
2022-10-20 19:22:50 +0200

    preise

---
+++ precache-manifest.bec89d8055f8e6c56e8058e7e2fd1ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afe34d5e3921b08284a5",
+    "url": "/static/js/main.afe34d5e.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "afe34d5e3921b08284a5",
+    "url": "/static/css/main.45bb0ef1.chunk.css"
+  },
+  {
+    "revision": "b78855d97265db0ef5d086658bcbcfdd",
+    "url": "/index.html"
+  }
+];
