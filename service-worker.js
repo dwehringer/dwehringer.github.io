6f7cfbf2efc469293406f034545b6b817ea9fbc4
dwehringer <36534515+dwehringer@users.noreply.github.com>
2023-09-14 20:50:21 +0200

    x

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f086ba66c5e8e150d89fd7386db1f6f0.js"
+  "/precache-manifest.e5c4a1d8fede5e7830a32a9dd5531d7a.js"
 );
 
 workbox.clientsClaim();
